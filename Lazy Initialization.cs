e85e7cb7e0ecc9711ff741f29986271d590d6173
aniavdalyan13 <ani.avdalyan13@icloud.com>
2026-05-18 21:24:19 +0400

    Add files via upload

---
+++ Lazy Initialization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Lazy_Demo
+{
+    class Database
+    {
+        public SqlConnection Connection { get; }
+
+        public Database()
+        {
+            Console.WriteLine("Connecting to DB...");
+            Connection = new SqlConnection("Server=.;Database=TestDB;");
+            Console.WriteLine("DB initialized");
+        }
+    }
+
+    class Program
+    {
+        static Lazy<Database> db = new Lazy<Database>(() => new Database());
+
+        static void Main()
+        {
+            Console.WriteLine("Program started");
+
+            Console.WriteLine("DB not created yet");
+
+            var conn = db.Value.Connection;
+
+            Console.WriteLine("DB is now ready");
+        }
+    }
+}
